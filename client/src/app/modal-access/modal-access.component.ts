f777318712487dc9a27a8f41344878428405a6a3
fallouseven <oussou.dieng@gmail.com>
2021-04-21 00:36:36 -0400

    add page access

---
+++ client/src/app/modal-access/modal-access.component.ts
@@ -0,0 +1,46 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+
+
+export interface DialogDataAccess {
+  langue: string;
+  id:number;
+  numeroIdentification: string;
+  nom: string;
+  isLogin:boolean;
+}
+
+@Component({
+  selector: 'app-modal-access',
+  templateUrl: './modal-access.component.html',
+  styleUrls: ['./modal-access.component.scss']
+})
+export class ModalAccessComponent implements OnInit {
+  cookieLogin: string;
+  numeroIdentification: string;
+  nom: string;
+  langue: string = "fr";
+ 
+
+  constructor(private translate: TranslateService,
+    public dialogRef: MatDialogRef<ModalAccessComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogDataAccess) {
+    this.translate.setDefaultLang('fr');
+    dialogRef.disableClose = true;
+  }
+
+  ngOnInit() {
+    this.data.isLogin = false;
+  }
+
+  commencer() {
+    this.data.isLogin = false;
+    this.dialogRef.afterOpened();
+  }
+
+  radioChange(choice) {
+    this.langue = choice.value;
+    this.translate.setDefaultLang(choice.value);
+  }
+}
